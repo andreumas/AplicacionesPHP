75c3c1f366a094904fdfa810ec8ac8386d21e799
andreumas <andreumasandres1995@gmail.com>
2017-05-03 12:36:04 +0200

    A25

---
+++ A25/lib/seguridad.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ *
+ */
+class seguridad 
+{
+	private $usuarios=null;
+	
+  function __construct()
+  {
+    session_start();
+	if(isset($_SESSION['usuario'])){ $this->usuarios=$_SESSION['usuario'];
+	}
+
+  }
+  
+  public function getUsuario(){
+	return $this->usuarios;
+  }
+  
+  public function addUsuario($usuario){
+	$_SESSION['usuario']=$usuario;
+	$this->usuarios=$usuario;
+  }
+  
+  public function logout(){
+    $_SESSION=[];
+	  session_destroy();
+  }
+}
+  ?>
